7dd08627b00491a907c285a526bbd602d4a38321
mobin <mobin.banik@gmail.com>
2024-08-16 05:22:27 -0400

    modify scraper and models.

--- doctor/models.py
+++ doctor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.html import mark_safe
 
+
 def get_doctor_path(instance, file_name=None):
     return "data/doctors/{0}/{1}/{2}/{3}".format(
         instance.city,
@@ -21,27 +22,64 @@ def get_doctor_path_for_image(instance, file_name=None):
 
 # Create your models here.
 class Doctor(models.Model):
-    profile_url = models.URLField()
-    source_page = models.TextField()
-    name = models.CharField(max_length=255)
-    expertise_category = models.IntegerField()
+
+    # POST
+    # https://nobat.ir/api/public/turn/daylist
+    # body :
+    # {
+    #     nice_id: dr.maryam.motamedinasab
+    #     office_id: 314150
+    # }
+
+    # POST
+    # https://nobat.ir/api/public/turn/scheduleinfo
+    # body :
+    # {
+    #     nice_id: dr.maryam.motamedinasab
+    #     office_id: 314150
+    #     day_key: 14030528
+    # }
+
+    # POST
+    # https://nobat.ir/api/public/turn/doctor
+    # body :
+    # {
+    #     nice_id: "dr.maryam.motamedinasab"
+    # }
+
+    # Site Meta Data
+    data_drid = models.CharField(max_length=255, null=True, blank=True) # OK!
+    data_niceid = models.CharField(max_length=255, null=True, blank=True) # OK!
+    # data_officeid = models.CharField(max_length=255, null=True, blank=True)
+
+    # Doctor data
+    name = models.CharField(max_length=255) # OK!
     city = models.IntegerField()
     city_name = models.CharField(max_length=255)
     neighborhood = models.IntegerField(null=True, blank=True)
     neighborhood_name = models.CharField(max_length=255, null=True, blank=True)
-    expertise_name_1 = models.CharField(max_length=255)
-    expertise_name_2 = models.CharField(max_length=255, null=True, blank=True)
-    nezam_number = models.IntegerField()
+    expertise_category = models.IntegerField()
+    expertise_category_name = models.CharField(max_length=255, null=True, blank=True)
+    expertise_name_1 = models.CharField(max_length=255) # OK!
+    expertise_name_2 = models.CharField(max_length=255, null=True, blank=True) # OK!
+    nezam_number = models.IntegerField() # OK!
     # appointment_type = models.CharField()
-    education_level = models.CharField(max_length=255)
-    is_verified = models.BooleanField(default=False)
-    doctor_image = models.ImageField(upload_to=get_doctor_path, null=True, blank=True)
-    like_count = models.IntegerField(default=0)
-    comment_link = models.URLField(null=True, blank=True)
-    instagram_link = models.URLField(null=True, blank=True)
-    extra_info = models.TextField(null=True, blank=True)
-    office_data = models.JSONField()
-    comments_data = models.JSONField()
+    education_level = models.CharField(max_length=255) # OK!
+    is_verified = models.BooleanField(default=False) # OK!
+    doctor_image = models.ImageField(upload_to=get_doctor_path, null=True, blank=True) # OK! BUT JUST URL
+    like_count = models.IntegerField(default=0) # OK!
+    comment_link = models.URLField(null=True, blank=True) # OK!
+
+    extra_info = models.TextField(null=True, blank=True) # OK!
+    office_data = models.JSONField(null=True, blank=True)
+    comments_data = models.JSONField(null=True, blank=True)
+    social_media = models.JSONField(null=True, blank=True) # OK!
+
+    # Meta data
+    scraped_date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    profile_url = models.URLField()
+    source_page = models.TextField()
 
     def add_comment(self, star: int, text: str, date: str):
         json_comments = self.comments_data if self.comments_data else []
@@ -54,21 +92,37 @@ class Doctor(models.Model):
         self.comments_data = json_comments
         self.save()
 
+    # POST
+    # https://nobat.ir/api/public/doctor/office/tells
+    # body :
+    # {
+    #     "office_id": "240915"
+    # }
     def add_office_data(
             self,
+            office_id: int,
+            office_type: str,
             office_title: str,
+            office_city: str,
             office_address: str,
             office_description: str,
             office_holiday: str,
             office_phone: list[str],
+            office_lat: str,
+            office_lon: str,
     ):
         json_office = self.office_data if self.office_data else []
         new_office_data = {
+            "office_type": office_type,
             "office_title": office_title,
+            "office_city": office_city,
             "office_address": office_address,
             "office_description": office_description,
             "office_holiday": office_holiday,
             "office_phone": office_phone,
+            "office_id": office_id,
+            "office_lat": office_lat,
+            "office_lon": office_lon,
         }
 
         # Append new entry to the list
--- doctor/scraper.py
+++ doctor/scraper.py
@@ -5,57 +5,167 @@ from .models import Doctor
 
 
 def get_comments(text):
+    # TODO get comments
     pass
+
+
+def parse_office(text: str, office_type: str):
+    soup = bs4.BeautifulSoup(text, 'html.parser')
+
+    _office_id = soup.find('div', attrs={'class': 'office'})
+    if _office_id:
+        _office_id = _office_id['data-officeid']
+
+    if office_type == 'visicall':
+        return {
+            'office_type': office_type,
+            'office_id': _office_id,
+        }
+
+    _office_title = soup.find('strong', attrs={'class': 'office-title'})
+    if _office_title:
+        _office_title = _office_title.text
+
+    _office_city_address_tag = soup.find('div', attrs={'class': 'office-address'})
+    _office_city = None
+    _office_address = None
+    if _office_city_address_tag:
+        _office_city_address_tag = _office_city_address_tag.find_all('strong')
+        _office_city = _office_city_address_tag[0].text
+        _office_address = _office_city_address_tag[1].text
+
+    _office_description = soup.find('div', attrs={'class': 'office-description'})
+    if _office_description:
+        _office_description = _office_description.text
+
+    _office_holiday = soup.find('div', attrs={'class': 'office-holiday'})
+    if _office_holiday:
+        _office_holiday = _office_holiday.text
+
+    _office_map_tag = soup.find('div', attrs={'class': 'office-map-container'})
+    _office_lat = None
+    _office_lon = None
+    if _office_map_tag:
+        _office_map_tag = _office_map_tag.find('div')
+        _office_lat = _office_map_tag['data-lat']
+        _office_lon = _office_map_tag['data-lng']
+
+    return {
+        'office_type': office_type,
+        'office_id': _office_id,
+        'office_title': _office_title,
+        'office_city': _office_city,
+        'office_address': _office_address,
+        'office_description': _office_description,
+        'office_holiday': _office_holiday,
+        'office_lat': _office_lat,
+        'office_lon': _office_lon,
+    }
 
 
 def scrape(text) -> dict[str, str]:
     soup = bs4.BeautifulSoup(text, 'html.parser')
     # print(soup)
+    # -\/-
+    _data = soup.find('div', attrs={'id': 'doctor'})
+    _data_drid = None
+    _data_niceid = None
+    if _data:
+        _data_drid = _data['data-drid']
+        _data_niceid = _data['data-niceid']
 
-    name = soup.find('h1', attrs={'class': 'doctor-ui-name'})
-    is_verified = False
-    if name.find('img'):
-        is_verified = True
-    name = name.find('span').text
+    # -\/-
+    _name = soup.find('h1', attrs={'class': 'doctor-ui-name'})
+    _is_verified = None
+    if _name:
+        if _name.find('img'):
+            _is_verified = True
+        else:
+            _is_verified = False
+        _name = _name.find('span').text
 
-    _specialties = soup.find_all('h2', attrs={'class': 'doctor-ui-specialty'})
-    specialties = list()
+    # -\/-
+    offices = soup.find_all('div', attrs={'class': 'office'})
+    offices_json_list = list()
+    if offices:
+        for office in offices:
+            office_type = 'normal'
+            if 'hide' in office['class']:
+                office_type = 'visicall'
+            json_data = parse_office(str(office), office_type)
+            offices_json_list.append(json_data)
+    # TODO get phone number and parse it
+    # -\/-
+    _nezam_number_tag = soup.find('div', attrs={'class': 'doctor-code'})
+    _doctor_code = None
+    _education_level = None
+    if _nezam_number_tag:
+        _nezam_number_tag = _nezam_number_tag.find_all('span')
+        _doctor_code = _nezam_number_tag[1].text
+        _education_level = _nezam_number_tag[0].text
 
-    for specialty in _specialties:
-        specialties.append(specialty.text)
+    # -\/-
+    _doctor_image_profile = soup.find('div', attrs={'class': 'doctor-ui-profile'})
+    if _doctor_image_profile:
+        _doctor_image_profile = _doctor_image_profile.find('img')
+        _doctor_image_profile = _doctor_image_profile['src']
 
-    doctor_code_tag = soup.find('div', attrs={'class': 'doctor-code'}).find_all('span')
-    doctor_code = doctor_code_tag[1].text
-    education_level = doctor_code_tag[0].text
+    # -\/-
+    _like_count = soup.find('span', attrs={'id': 'followers-count'})
+    if _like_count:
+        _like_count = _like_count.text
 
-    doctor_image_profile = soup.find('div', attrs={'class': 'doctor-ui-profile'})
-    doctor_image_profile = doctor_image_profile.find('img')
-    doctor_image_url = doctor_image_profile['src']
-    like_count = soup.find('span', attrs={'id': 'followers-count'}).text
+    # -\/-
+    _comment_link = soup.find('a', attrs={'class': 'btn btn-rounded btn-gray'})
+    if _comment_link:
+        _comment_link = _comment_link['href']
 
-    comment_link = soup.find('a', attrs={'class': 'btn btn-rounded btn-gray'})
-    comment_link = comment_link['href']
-
+    # -\/-
     extra_info_tag = soup.find('p', attrs={'class': 'nl-to-br'})
-    extra_info = str()
-    for info in extra_info_tag:
-        extra_info += info.text
+    _extra_info = str()
+    if extra_info_tag:
+        for info in extra_info_tag:
+            _extra_info += info.text
 
-    temp = Doctor.objects.create(
-        name=name,
-        is_verified=is_verified,
-    )
+    # -\/-
+    _social_media_tag = soup.find('div', attrs={'class': 'social-media'})
+    _social_media = list()
+    if _social_media_tag:
+        _social_media_tag = _social_media_tag.find_all('a')
+        for a in _social_media_tag:
+            _social_media.append(a['href'])
+
+    # -\/-
+    _expertise = soup.find_all('h2', attrs={'class': 'doctor-ui-specialty'})
+    _expertise_name_1 = None
+    _expertise_name_2 = None
+    if _expertise:
+        _expertise_name_1 = _expertise[0].text
+        if len(_expertise) > 1:
+            _expertise_name_2 = _expertise[1].text
+
+    # temp = Doctor.objects.create(
+    #     name=name,
+    #     is_verified=is_verified,
+    #
+    # )
     return {
-        'extra_info': extra_info,
-        'comment_link': comment_link,
-        'like_count': like_count,
-        'doctor_image_url': doctor_image_url,
-        'doctor_code': doctor_code,
-        'education_level': education_level,
-        'name': name,
-        'is_verified': is_verified,
-        'specialties': specialties,
+        'extra_info': _extra_info,
+        'comment_link': _comment_link,
+        'like_count': _like_count,
+        'doctor_image_url': _doctor_image_profile,
+        'doctor_code': _doctor_code,
+        'education_level': _education_level,
+        'name': _name,
+        'is_verified': _is_verified,
+        'specialties1': _expertise_name_1,
+        'specialties2': _expertise_name_2,
+        'data_drid': _data_drid,
+        'data_niceid': _data_niceid,
+        'offices': offices_json_list,
+        'social_media': _social_media,
     }
+
 
 
 def main():
